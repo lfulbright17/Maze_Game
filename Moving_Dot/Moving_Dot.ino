a8cb0b9bb6817325b7d779cd817767ca5fccc7da
lfulbright17 <lfulbright17@punahou.edu>
2014-11-17 15:10:46 -1000

    initial commit
    created a dot that can move to the right, and it can’t move off screen

---
+++ Moving_Dot/Moving_Dot.ino
@@ -0,0 +1,83 @@
+/*
+  Moving_Dot.pde
+ 
+ Example file using the The Meggy Jr Simplified Library (MJSL)
+  from the Meggy Jr RGB library for Arduino
+   
+ Blink a damned LED.
+   
+   
+ 
+ Version 1.25 - 12/2/2008
+ Copyright (c) 2008 Windell H. Oskay.  All right reserved.
+ http://www.evilmadscientist.com/
+ 
+ This library is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ 	  
+ */
+
+ 
+ 
+ 
+ 
+
+/*
+ * Adapted from "Blink,"  The basic Arduino example.  
+ * http://www.arduino.cc/en/Tutorial/Blink
+ */
+
+#include <MeggyJrSimple.h>    // Required code, line 1 of 2.
+
+//Global Variables go here
+int xcoord = 3;
+int ycoord = 4;
+
+void setup()                    // run once, when the sketch starts
+{
+  MeggyJrSimpleSetup();      // Required code, line 2 of 2.
+}
+
+void loop()                     // run over and over again
+{
+  CheckButtonsPress();
+  if (Button_Right)
+  {
+    if (xcoord < 7)
+      xcoord++;
+    if (xcoord > 7)
+      xcoord = 0;
+  }
+    
+   CheckButtonsPress();
+   if(Button_Left)
+  {
+    if (ycoord < 7)
+      ycoord++;
+    if (ycoord > 7)
+      ycoord = 0;
+    
+  }
+  
+  DrawPx(xcoord, ycoord, DimAqua); //Draw Dot
+  DisplaySlate();
+  delay(150);
+  ClearSlate();
+  
+  
+  
+  
+  
+}
+
+
