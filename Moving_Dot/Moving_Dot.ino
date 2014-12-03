2d1de7aa8ce7641535f83995b6834bf46a1a9d04
lfulbright17 <lfulbright17@punahou.edu>
2014-12-03 00:13:11 -1000

    Almost finished
    added in all 3 levels (4 including the winning screen). used a boolean
    to trigger the next levels without cheating thanks to mr kiang. just
    need to add a way to keep track of levels now.

--- Moving_Dot/Moving_Dot.ino
+++ Moving_Dot/Moving_Dot.ino
@@ -10,18 +10,21 @@
 //Global Variables go here
 int xcoord = 3;
 int ycoord = 4;
+int level = 1;
+
+
+
 
 void setup()                    // run once, when the sketch starts
 {
   MeggyJrSimpleSetup();   // Required code, line 2 of 2.
   SetAuxLEDs(0);
+  
 
 }
 
 void loop()                     // run over and over again
 {
-  
-  
   CheckButtonsPress();
   if (Button_Right)
   {
@@ -37,6 +40,38 @@ void loop()                     // run over and over again
         if(ReadPx(xcoord, ycoord)==Green)
         {
           GAMEOVER();
+        }
+        if(isThereBlack()==false)
+        { 
+          if(level==1)
+            {
+              Tone_Start(ToneC6, 100);
+              delay(100);
+            }
+          if(level==2)
+            {
+              Tone_Start(ToneC6, 100);
+              delay(100);
+            }
+          if(level==3)
+            {
+              Tone_Start(ToneC6, 100);
+              delay(100);
+            }
+          delay(100);
+          ClearSlate();
+          level++;
+          if(level==4)
+            {
+              WINNINGSCREEN();
+            }
+        }
+        if(ReadPx(xcoord, ycoord)==Orange)
+        {
+          if(isThereBlack()==true)
+          {
+            GAMEOVER();
+          }
         }
       }
     else
@@ -60,6 +95,38 @@ void loop()                     // run over and over again
         {
           GAMEOVER();
         }
+        if(isThereBlack()==false)
+        { 
+          if(level==1)
+            {
+              Tone_Start(ToneC6, 100);
+              delay(100);
+            }
+          if(level==2)
+            {
+              Tone_Start(ToneC6, 100);
+              delay(100);
+            }
+          if(level==3)
+            {
+              Tone_Start(ToneC6, 100);
+              delay(100);
+            }
+          delay(100);
+          ClearSlate();
+          level++;
+          if(level==4)
+            {
+              WINNINGSCREEN();
+            }
+        }
+        if(ReadPx(xcoord, ycoord)==Orange)
+        {
+          if(isThereBlack()==true)
+          {
+            GAMEOVER();
+          }
+        }
       }
     else
     {
@@ -81,10 +148,41 @@ void loop()                     // run over and over again
         if(ReadPx(xcoord, ycoord)==Green)
         {
           GAMEOVER();
-        }  //right here is where i would have the condition for if the whole screen is lit up
-           // to trigger level 2 (whole screen must be lit up, then step on orange dot) 
-     }  
-        
+        }
+        if(isThereBlack()==false)
+        { 
+          if(level==1)
+            {
+              Tone_Start(ToneC6, 100);
+              delay(100);
+            }
+          if(level==2)
+            {
+              Tone_Start(ToneC6, 100);
+              delay(100);
+            }
+          if(level==3)
+            {
+              Tone_Start(ToneC6, 100);
+              delay(100);
+            }
+          delay(100);
+          ClearSlate();
+          level++;
+          if(level==4)
+            {
+              WINNINGSCREEN();
+            }
+        }
+        if(ReadPx(xcoord, ycoord)==Orange)
+        {
+          if(isThereBlack()==true)
+          {
+            GAMEOVER();
+          }
+        }
+   
+      }   
     else
     {
       ycoord = 7;
@@ -106,6 +204,38 @@ void loop()                     // run over and over again
         {
           GAMEOVER();
         }
+        if(isThereBlack()==false)
+        { 
+          if(level==1)
+            {
+              Tone_Start(ToneC6, 100);
+              delay(100);
+            }
+          if(level==2)
+            {
+              Tone_Start(ToneC6, 100);
+              delay(100);
+            }
+          if(level==3)
+            {
+              Tone_Start(ToneC6, 100);
+              delay(100);
+            }
+          delay(100);
+          ClearSlate();
+          level++;
+          if(level==4)
+            {
+              WINNINGSCREEN();
+            }
+        }
+        if(ReadPx(xcoord, ycoord)==Orange)
+        {
+          if(isThereBlack()==true)
+          {
+            GAMEOVER();
+          }
+        }
       }
     else
     {
@@ -113,18 +243,39 @@ void loop()                     // run over and over again
     }     
   }
   
-  START();
-  DisplaySlate();
+
+  if(level==1)
+    {
+      START();
+    }
+
+  if(level==2)
+    {
+      level2();
+    }
   
+  if(level==3)
+    {
+      level3();
+    }
   if(ReadPx(xcoord+1, ycoord+1)==0);
     {
       DrawPx(xcoord, ycoord, Green);
     }
+  
+  
+  
+ 
+  
+  
+  
+  
+  
    
-
 }
 
-void OBSTACLES()
+
+void OBSTACLES1()
 {
   DrawPx(2, 4, Red); 
   DrawPx(2, 3, Red);
@@ -145,14 +296,63 @@ void OBSTACLES()
   DrawPx(6, 7, Red);
 }
 
-void ENDONE()
+void OBSTACLES2()
+{
+  DrawPx(1, 3, Red);
+  
+  DrawPx(2, 3, Red);
+  DrawPx(2, 5, Red);
+  
+  DrawPx(3, 1, Red);
+  DrawPx(3, 2, Red);
+  DrawPx(3, 3, Red);
+  DrawPx(3, 5, Red);
+  
+  DrawPx(4, 3, Red);
+  DrawPx(4, 4, Red);
+  DrawPx(4, 5, Red);
+}
+
+void OBSTACLES3()
+{
+  DrawPx(0, 6, Red);
+  DrawPx(0, 7, Red);
+  
+  DrawPx(1, 6, Red);
+  DrawPx(1, 7, Red);
+  
+  DrawPx(3, 2, Red);
+  DrawPx(3, 3, Red);
+  
+  DrawPx(4, 4, Red);
+  
+  DrawPx(5, 5, Red);
+  DrawPx(5, 6, Red);
+  
+  DrawPx(6, 6, Red);
+  
+  DrawPx(7, 2, Red);
+
+}
+
+void END()
 {
   DrawPx(7, 7, Orange); 
+}
+void END2()
+{
+  DrawPx(3, 4, Orange); 
+}
+void END3()
+{
+  DrawPx(7, 4, Orange);
 }
 
 void ENEMIES()
 {
-  DrawPx(3, 2, Blue); 
+  DrawPx(3, 2, Blue);
+   delay(100);
+  ClearSlate(); 
 }
 
 void GAMEOVER()
@@ -164,38 +364,118 @@ void GAMEOVER()
   Tone_Start(ToneA5, 100);
   delay(1000);
   ClearSlate();
-  NEWSTART();
+  if(level==1)
+  {
+    NEWSTART1();
+  }
+  if(level==2)
+  {
+    NEWSTART2();
+  }
+  if(level==3)
+  {
+    NEWSTART3();
+  }
 }
+
+void WINNINGSCREEN()
+{
+  Tone_Start(ToneA5, 100);
+  delay(1000);
+  Tone_Start(ToneA5, 100);
+  delay(1000);
+  Tone_Start(ToneA5, 100);
+  delay(1000);
+  NEWSTART1();
+  DrawPx(xcoord, ycoord, DimAqua);
+  level = 1;
+  if(level = 1)
+    xcoord=3;
+    ycoord=4;
+}
+  
 
 void START()
 {
   DrawPx(xcoord, ycoord, DimAqua);
   DisplaySlate();
-  OBSTACLES();
+  END();
   DisplaySlate();
-  ENDONE();
+  OBSTACLES1();
   DisplaySlate();
 }
 
-void NEWSTART()
+void NEWSTART1()
 {
   ClearSlate();
   xcoord = 3;
   ycoord = 4;
   DisplaySlate();
-  OBSTACLES();
+  OBSTACLES1();
   DisplaySlate();
-  ENDONE();
+  END();
   DisplaySlate();
 }
 
-void seconddrawSKYTHREE() //WHERE LEVEL TWO SHOULD GO!!!
+void NEWSTART2()
 {
-  for(int o=0; o<8; o++)
-    DrawPx(o, 1, Blue);
-  for(int s=0; s<8; s++)
-    DrawPx(s, 0, Blue);
+  ClearSlate();
+  xcoord = 7;
+  ycoord = 7;
+  DisplaySlate();
+  OBSTACLES2();
+  DisplaySlate();
+  END2();
+  DisplaySlate();
+}
+
+void NEWSTART3()
+{
+  ClearSlate();
+  xcoord = 3;
+  ycoord = 4;
+  DisplaySlate();
+  OBSTACLES3();
+  DisplaySlate();
+  END3();
+  DisplaySlate();
+}
+
+void level2()
+{
+  DrawPx(xcoord, ycoord, DimAqua);
+  DisplaySlate();
+  END2();
+  DisplaySlate();
+  OBSTACLES2();
+  DisplaySlate();
+  
 } 
 
+void level3()
+{
+  DrawPx(xcoord, ycoord, DimAqua);
+  DisplaySlate();
+  END3();
+  DisplaySlate();
+  OBSTACLES3();
+  DisplaySlate();
+  
+} 
+
+boolean isThereBlack()
+{
+  for (int x = 0; x < 8; x++)
+    {
+      for (int y = 0; y < 8; y++) 
+      {
+        if(ReadPx(x,y)==0)
+          return true;
+      }
+    } 
+  return false;
+}
+     
+  
 
 
